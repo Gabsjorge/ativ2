35e451a61de7af153c8895d3e437cba98ccc7a7a
gabi <gabs.luluzinha@gmail.com>
2018-09-16 22:42:59 -0300

    Adicionando files do processo de compilar modulo hello-1 e Makefile

---
+++ .hello-1.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/gabi/ativ2/hello-1.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/gabi/ativ2/hello-1.ko /home/gabi/ativ2/hello-1.o /home/gabi/ativ2/hello-1.mod.o ;  true
---
+++ .tmp_versions/hello-1.mod
@@ -0,0 +1,3 @@
+/home/gabi/ativ2/hello-1.ko
+/home/gabi/ativ2/hello-1.o
+
