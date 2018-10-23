ace31083f2271a1a5c92829a8d98a607011a5ed1
Gabsjorge <42352564+Gabsjorge@users.noreply.github.com>
2018-10-22 23:09:01 -0300

    Delete .hello-3.ko.cmd

--- .hello-3.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/gabi/ativ2/hello-3.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/gabi/ativ2/hello-3.ko /home/gabi/ativ2/hello-3.o /home/gabi/ativ2/hello-3.mod.o ;  true
