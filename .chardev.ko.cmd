e96a47043e517307ec0d647765efa409b331eb01
Gabsjorge <42352564+Gabsjorge@users.noreply.github.com>
2018-10-22 23:04:27 -0300

    Delete .chardev.ko.cmd

--- .chardev.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/gabi/ativ2/chardev.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/gabi/ativ2/chardev.ko /home/gabi/ativ2/chardev.o /home/gabi/ativ2/chardev.mod.o ;  true
